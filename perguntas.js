c10c389b78f568b44ff94d2172598c56b484d92d
isabele picksius <106542913+barbiebutterfly@users.noreply.github.com>
2024-08-23 16:18:34 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,117 +1,173 @@
 export const perguntas = [
     {
-        enunciado: "1. Qual é a sua refeição favorita do dia?",
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
-                texto: "A) Café da manhã",
+                texto: "Isso é assustador!",
                 afirmacao: [
-                    " O Tradicionalista. Você é alguém que valoriza a rotina e gosta de conforto. Na culinária, isso se reflete em sua preferência por refeições familiares e sabores clássicos. Você gosta de manter as coisas simples e tende a ser uma pessoa confiável e calma."
+                    "No início ficou com medo do que essa tecnologia pode fazer.",
+                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
                 ],
                 proxima: 1,
             },
             {
-                texto: "B) Almoço",
+                texto: "Isso é maravilhoso!",
                 afirmacao: [
-                    "O Aventureiro. Você adora experimentar novos sabores e é conhecido por ser curioso e ousado na vida e na cozinha. Seja um prato novo ou uma combinação inusitada de temperos, você não tem medo de se arriscar. Sua personalidade é extrovertida e aberta a novidades."
+                    "Quis saber como usar IA no seu dia a dia.",
+                    "Pensou que IA pode ajudar em tarefas da sua vida."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "2. Qual tipo de cozinha você prefere?",
+        enunciado: "Utilizar uma IA pode ser aterrorizante mesmo, e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Brasileira (feijoada, churrasco)",
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: [
-                    "O Tradicionalista. Você é alguém que valoriza a rotina e gosta de conforto. Na culinária, isso se reflete em sua preferência por refeições familiares e sabores clássicos. Você gosta de manter as coisas simples e tende a ser uma pessoa confiável e calma.",
-    
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
                 ],
                 proxima: 3,
             },
             {
-                texto: " Italiana (massas, pizzas)",
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
                 afirmacao: [
-                    "O Equilibrado. Você busca equilíbrio em tudo o que faz, e isso se reflete no seu gosto pela culinária. Gosta de refeições bem preparadas, com sabores harmônicos e de qualidade. Na vida, você é uma pessoa que busca estabilidade e harmonia, tanto no trabalho quanto nas relações."
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Quando você vai a um restaurante, você:",
+        enunciado: "Um chat com IA pode ser uma boa alternativa para realizar tarefas do dia a dia e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Sempre pede o mesmo prato",
+                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: [
-                    "O Tradicionalista. Você é alguém que valoriza a rotina e gosta de conforto. Na culinária, isso se reflete em sua preferência por refeições familiares e sabores clássicos. Você gosta de manter as coisas simples e tende a ser uma pessoa confiável e calma."
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
                 ],
                 proxima: 3,
             },
             {
-                texto: "Escolhe algo que viu nas redes sociais",
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
                 afirmacao: [
-                    " O Equilibrado. Você busca equilíbrio em tudo o que faz, e isso se reflete no seu gosto pela culinária. Gosta de refeições bem preparadas, com sabores harmônicos e de qualidade. Na vida, você é uma pessoa que busca estabilidade e harmonia, tanto no trabalho quanto nas relações."
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: " Qual é sua sobremesa favorita?",
+        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
         alternativas: [
             {
-                texto: " Sorvete",
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
-                    "O Tradicionalista. Você é alguém que valoriza a rotina e gosta de conforto. Na culinária, isso se reflete em sua preferência por refeições familiares e sabores clássicos. Você gosta de manter as coisas simples e tende a ser uma pessoa confiável e calma."
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Pudim",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: [
-                    " O Intenso. Seus gostos são fortes e definidos, tanto na culinária quanto na vida. Você gosta de viver tudo com intensidade e paixão. Temperos marcantes e pratos ousados chamam sua atenção, assim como situações desafiadoras e emoções profundas."
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
                 proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Qual tipo de cozinha você prefere?",
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
         alternativas: [
             {
-                texto: "Japonesa (sushi, sashimi)",
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
-                    "O Aventureiro. Você adora experimentar novos sabores e é conhecido por ser curioso e ousado na vida e na cozinha. Seja um prato novo ou uma combinação inusitada de temperos, você não tem medo de se arriscar. Sua personalidade é extrovertida e aberta a novidades."
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
-                proxima: 7,
+                proxima: 5,
             },
             {
-                texto: "Mexicana (tacos, nachos)",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: [
-                    "O Intenso. Seus gostos são fortes e definidos, tanto na culinária quanto na vida. Você gosta de viver tudo com intensidade e paixão. Temperos marcantes e pratos ousados chamam sua atenção, assim como situações desafiadoras e emoções profundas.O que você achou desse formato? Quer ajustar algo no quiz?"
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
-                proxima: 8,
+                proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Como você gosta de temperar a comida?",
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
             {
-                texto: " Leve, com poucas ervas e sal",
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
-                    "O Aventureiro. Você adora experimentar novos sabores e é conhecido por ser curioso e ousado na vida e na cozinha. Seja um prato novo ou uma combinação inusitada de temperos, você não tem medo de se arriscar. Sua personalidade é extrovertida e aberta a novidades."
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
                 ],
-                proxima: 9,
+                proxima: 7,
             },
             {
-                texto: "Bem temperada, com bastante especiarias",
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
                 afirmacao: [
-                    "O Aventureiro. Você adora experimentar novos sabores e é conhecido por ser curioso e ousado na vida e na cozinha. Seja um prato novo ou uma combinação inusitada de temperos, você não tem medo de se arriscar. Sua personalidade é extrovertida e aberta a novidades."
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
                 ],
-                proxima: 10,
+                proxima: 7,
             },
         ]
     },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: [
+                    "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
+                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                ],
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: [
+                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
+                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
+                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                ],
+            },
+        ]
+    }
 ];
